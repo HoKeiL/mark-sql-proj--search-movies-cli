817d4e6bb84ed7b129985598e85b6d13b713f656
Your Name <you@example.com>
2023-08-15 16:29:59 +0100

    version 1 completed

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,8 +1,35 @@
-import { question } from "readline-sync";
-import { Client } from "pg";
+// import { question } from "readline-sync";
+
+// import { Client } from "pg";
+const { Client } = require("pg");
+const { question } = require("readline-sync")
+
 
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
-console.log("Welcome to search-movies-cli!");
+
+async function searchMovies() {
+    const client = new Client({ database: 'omdb' });
+    console.log("Welcome to search-movies-cli!");
+    const userName = question('May I have your name? ');
+    console.log('Hi ' + userName + '!');
+    await client.connect();
+    let searchTerm;
+    while (searchTerm !== 'q') {
+        searchTerm = question("Search for what movie?(or 'q' to quit): ");
+        if (searchTerm === 'q') {
+            console.log('Search ended');
+            await client.end()
+        } else {
+            console.log('Searching ' + searchTerm + '...');
+            const text = 'SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count from movies WHERE name iLIKE $1 AND kind = \'movie\' ORDER BY date DESC LIMIT 10 '
+            const values = [`%${searchTerm}%`];
+            const result = await client.query(text, values)
+            console.table(result.rows)
+        }
+    }
+    await client.end()
+}
+
+searchMovies();
