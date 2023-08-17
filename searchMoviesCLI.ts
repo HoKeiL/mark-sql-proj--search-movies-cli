ef8570ab142c08e266f39d3c35233d699f7d4ffd
Your Name <you@example.com>
2023-08-17 13:41:42 +0100

    version 2 - keep track of favourite movies, currently displaying favourites table only in favourite, not yet dnoe join with movie table

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -14,7 +14,7 @@ const { question } = require("readline-sync")
 async function searchMovies() {
     const client = new Client({ database: 'omdb' });
     console.log("Welcome to search-movies-cli!");
-    const action = question('what would you like to do? \n[1] search \n[2] see favourite \n[3] quit \n');
+    const action = question('What would you like to do? \n[1] search \n[2] see favourite \n[3] quit \n');
 
     if (action === '1') {
         console.log(`OK! Let's search!`)
@@ -26,21 +26,43 @@ async function searchMovies() {
                 await client.end();
                 console.log('Search ended');
             } else {
+                //search and show results
                 console.log('Searching ' + searchTerm + '...');
                 const text = 'SELECT id, name, date, runtime, budget, revenue, vote_average, votes_count from movies WHERE name iLIKE $1 AND kind = \'movie\' ORDER BY date DESC LIMIT 10 '
                 const values = [`%${searchTerm}%`];
                 const result = await client.query(text, values);
                 console.table(result.rows);
+
+                //get movie name and id
+                const movieList = result.rows.map((row: any) => { return { id: row.id, name: row.name } })
+
+                //ask user to select movie and save to favourites
+                const movieNameFromList = movieList.map((movie: any) => movie.name)
+                const addFav = keyInSelect(movieNameFromList, "Whcih movie would you like to add to favourite?")
+
+                //insert selected movie to favourite DB
+                console.log("Adding " + movieNameFromList[addFav] + " to favourites...")
+                const addFavText = "INSERT INTO favourites (movie_id, name) VALUES ($1, $2)"
+                const addFaValues = [movieList[addFav].id, movieList[addFav].name];
+                const addFavQuery = await client.query(addFavText, addFaValues);
+
+                //show on console and comfirm movie has been added to favourites
+                const addFavResult = await client.query("SELECT * from favourites");
+                console.table(addFavResult.rows);
+                console.log("Added " + movieNameFromList[addFav] + " to favourites!")
             }
+
         }
         await client.end()
 
     } else if (action === '2') {
         console.log(`OK! Let's see favourite!`)
         await client.connect();
-        const text = 'SELECT id, name from favourites LIMIT 10 '
+        const text = 'SELECT * from favourites LIMIT 10 '
         const result = await client.query(text)
         console.table(result.rows)
+        await client.end()
+        console.log('Session ended');
     } else {
         await client.end()
         console.log('Session ended');
